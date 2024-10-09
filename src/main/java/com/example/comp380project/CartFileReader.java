00feaa6a8a41578c90706e6dd3e42b281e5a4db7
Alfredo Ulloa <alfredo.ulloa.58@my.csun.edu>
2024-10-09 14:20:49 -0700

    created file

---
+++ src/main/java/com/example/comp380project/CartFileReader.java
@@ -0,0 +1,64 @@
+package com.example.comp380project;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CartFileReader {
+    private static final String CSV_CART_INFO = "data/ItemsInCart.csv";
+
+    public void saveCart(Cart cart) throws IOException, ClassNotFoundException {
+        List<Cart> existingCarts = retrieveAllCarts();
+
+        for (Cart existingCart : existingCarts) {
+            if (existingCart != null) {
+                if (existingCart.getIdCart() == cart.getIdCart()) {
+                    System.out.println("Cart " + cart.getIdCart() + " for customer " + cart.getCustomer().getFirstName() + " already exists. Not saving.");
+                    return;
+                }
+            }
+        }
+        try (PrintWriter writer = new PrintWriter(new FileWriter(CSV_CART_INFO, true))) {  // Append mode
+            writer.println(cartToCSV(cart));
+        }
+    }
+
+    public List<Cart> retrieveAllCarts() throws FileNotFoundException, IOException, ClassNotFoundException {
+        List<Cart> carts = new ArrayList<>();
+        File file = new File(CSV_CART_INFO);
+
+        if (!file.exists()) {
+            return carts;
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                carts.add(csvToCart(line));
+            }
+        }
+        return carts;
+    }
+
+    private Cart csvToCart(String line){
+        String[] data = line.split(",");
+        if(data[0].equalsIgnoreCase("id")) return null;
+
+        int id = Integer.parseInt(data[0]);
+        Customer customer = new CustomerFileReader().csvToCustomer(data[1]);
+        Map<Item,Integer> items = data[2];
+
+        return new Cart(id, customer, items);
+    }
+
+    private String cartToCSV(Cart cart){
+        return cart.getIdCart() +
+                "," + cart.getCustomer() +
+                "," + cart.getItems();
+    }
+
+    private Map<Item, Integer> parseItems (String line){
+
+    }
+
+}
