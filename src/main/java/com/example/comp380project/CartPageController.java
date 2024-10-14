9d399b9a08abe1667c5404eea4767b67461c7249
jaacobv <jacob.valmeo.146@my.csun.edu>
2024-10-14 14:25:43 -0700

    Update and rename CartController.java to CartPageController.java

--- src/main/java/com/example/comp380project/CartPageController.java
+++ src/main/java/com/example/comp380project/CartPageController.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class CartController {
+public class CartPageController {
     private Stage stage;
     private Scene scene;
     private Parent root;
