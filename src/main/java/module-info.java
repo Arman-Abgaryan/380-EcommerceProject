8e84aad17d61a053c90e4c45d80477c7696ab239
jaacobv <jacob.valmeo.146@my.csun.edu>
2024-10-27 11:33:43 -0700

    Delete src/main/java/module-info.java

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.comp380project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.comp380project to javafx.fxml;
-    exports com.example.comp380project;
-}
