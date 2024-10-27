e7a69956071b48e9d400b7bfae21ec99b01f23cd
jaacobv <jacob.valmeo.146@my.csun.edu>
2024-10-27 11:44:23 -0700

    Delete 380-EcommerceProject-main-v2.5 directory

--- 380-EcommerceProject-main-v2.5/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.comp380project {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.comp380project to javafx.fxml;
-    exports com.example.comp380project;
-}
