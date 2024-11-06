2592707206536990a821b8123bb907768ead33c5
ArmanAbgaryan <arman.abgaryan.091@my.csun.edu>
2024-11-06 13:12:32 -0800

    Added SweaterPage method and import statements

--- src/main/java/com/example/comp380project/SweaterPage.java
+++ src/main/java/com/example/comp380project/SweaterPage.java
@@ -1,4 +1,39 @@
 package com.example.comp380project;
 
+import javafx.stage.Stage;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+import java.awt.event.ItemEvent;
+import java.io.IOException;
+import java.util.List;
+
 public class SweaterPage {
+
+    private Storefront storefront;
+    private Cart cart;
+    private Stage primaryStage;
+
+
+    public SweaterPage(Storefront storefront, Cart cart,Stage primaryStage) {
+        this.storefront = storefront;
+        this.cart = cart;
+        this.primaryStage = primaryStage;
+    }
+
+
+
 }
