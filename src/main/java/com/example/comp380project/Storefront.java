b0d6616a574c7dc5dba4fb02ed817290f657ead9
Alfredo Ulloa <alfredo.ulloa.58@my.csun.edu>
2024-11-12 15:28:55 -0800

    removed unecessary imports

--- src/main/java/com/example/comp380project/Storefront.java
+++ src/main/java/com/example/comp380project/Storefront.java
@@ -8,14 +8,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Storefront extends Application {
