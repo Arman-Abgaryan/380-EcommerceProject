a1e0d33c2632aa695b91fda9d1d68778fe757737
jaacobv <jacob.valmeo.146@my.csun.edu>
2024-11-18 13:51:30 -0800

    Add files via upload

--- src/main/java/com/example/comp380project/ItemPage.java
+++ src/main/java/com/example/comp380project/ItemPage.java
@@ -6,18 +6,32 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import java.util.List;
 
+/**
+ * Class that creates and holds GUI for ItemPage which displays Items
+ */
 public class ItemPage {
 
     private Storefront storefront;
     private Cart cart;
     private Stage primaryStage;
 
+    /**
+     * Constructor that holds contents included in ItemPage
+     * @param storefront
+     * @param cart
+     * @param primaryStage
+     */
     public ItemPage(Storefront storefront, Cart cart, Stage primaryStage){
         this.storefront = storefront;
         this.cart = cart;
         this.primaryStage = primaryStage;
     }
 
+    /**
+     * Method that creates interactable GUI interface that displays Items
+     * @param type
+     * @return Scene
+     */
     public Scene getItemPage(String type){
         TopBoxFactory topBoxFactory = new TopBoxFactory(storefront, cart, primaryStage);
         SearchBoxFactory searchBoxFactory = new SearchBoxFactory(storefront, cart, primaryStage);
