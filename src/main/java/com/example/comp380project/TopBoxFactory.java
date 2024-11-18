f0566db58db6b01929885f5dab81942dbdb15b4e
ArmanAbgaryan <arman.abgaryan.091@my.csun.edu>
2024-11-18 13:49:12 -0800

    Added javadoc comments, removed unused imports, and also adjusted spacing

--- src/main/java/com/example/comp380project/TopBoxFactory.java
+++ src/main/java/com/example/comp380project/TopBoxFactory.java
@@ -1,30 +1,31 @@
 package com.example.comp380project;
 
-
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
-import java.util.List;
-
-
+/**
+ * Class that creates the top section of the GUI that contains the back to home button, logo, and cart button
+ */
 public class TopBoxFactory {
     private Storefront storefront;
     private Cart cart;
     private Stage primaryStage;
 
 
+    /**
+     * Default constructor for creating the top section of the GUI
+     * @param storefront
+     * @param cart
+     * @param primaryStage
+     */
     public TopBoxFactory(Storefront storefront, Cart cart, Stage primaryStage) {
         this.storefront = storefront;
         this.cart = cart;
@@ -32,9 +33,18 @@ public class TopBoxFactory {
     }
 
 
+    /**
+     * Creates a BorderPane called topSection for the elements at the top of each page in the GUI
+     *
+     * topSection includes:
+     * - "Back to Home" button that returns the user to the Storefront when pressed
+     * - The AJAD Logo that also acts as a "Back to Home" button
+     * - "Cart" button that navigates the user to their respective cart when clicked
+     *
+     * @return topSection
+     */
     public BorderPane getTopSection() {
         BorderPane topSection = new BorderPane();
-
 
         // Back to Home Button
         Button backButton = new Button("Back to Home");
@@ -44,9 +54,6 @@ public class TopBoxFactory {
         backButton.setOnAction(e -> {
             returnToStorefront();
         });
-
-
-
 
         // Logo
         Image logo = new Image(getClass().getResourceAsStream("/AJAD Edited Logo.png"));
@@ -63,15 +70,11 @@ public class TopBoxFactory {
             }
         });
 
-
         // Cart Button
         Image cartImage = new Image(getClass().getResourceAsStream("/cart.png"));
         ImageView cartImageView = new ImageView(cartImage);
         cartImageView.setFitWidth(30);
         cartImageView.setFitHeight(30);
-
-
-
 
         Button cartButton = new Button("Cart");
         cartButton.setStyle("-fx-background-color: white;");
@@ -83,15 +86,22 @@ public class TopBoxFactory {
         topSection.setRight(cartButton);
         BorderPane.setMargin(cartButton, new Insets(22, 30, 0, 0));
 
-
         return topSection;
     }
 
+
+    /**
+     * Method that handles returning the user to the Storefront page
+     */
     private void returnToStorefront() {
         storefront.createStoreFront();
         primaryStage.setScene(storefront.getScene());
     }
 
+
+    /**
+     * Method that handles navigating the user to their respective cart
+     */
     private void goToCartPage() {
         CartPage cartPage = new CartPage(cart,storefront);
         primaryStage.setScene(cartPage.getCartScene(primaryStage));
